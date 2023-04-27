fa5a54f4b60036e16416d95b7fe9b83bc140f9e5
shun125 <dna25r1@gmail.com>
2023-04-27 12:21:24 +0800

    Frist commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const title = document.querySelector("h1");
+const img = document.querySelector("h2");
+setTimeout(()=>{
+    title.innerHTML="MOM HAPPY BIRTHDAY!!!"
+    img.innerHTML=`<img src="./img/birthday-cake-600x600.jpg" class="mt-4" alt=""></img>`
+},3000)
